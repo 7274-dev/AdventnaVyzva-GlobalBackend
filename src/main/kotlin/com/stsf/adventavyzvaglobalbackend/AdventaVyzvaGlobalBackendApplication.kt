2b251019a7e3b9603d3368bf0b9e3dcdac007bda
Magiccop990 <jtulek@at-rt.com>
2021-04-27 19:18:26 +0200

    :tada: Resets repo to a base project

---
+++ src/main/kotlin/com/stsf/adventavyzvaglobalbackend/AdventaVyzvaGlobalBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.stsf.adventavyzvaglobalbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdventaVyzvaGlobalBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<AdventaVyzvaGlobalBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/stsf/adventavyzvaglobalbackend/AdventaVyzvaGlobalBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.stsf.adventavyzvaglobalbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdventaVyzvaGlobalBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
