67af85b9ff5ddba3c3a2d38fdf0cd73c89131476
MagSG-7274 <49731971+MagSG-7274@users.noreply.github.com>
2021-05-01 21:13:34 +0200

    :art: Adds fields for first and last name

--- src/main/kotlin/com/stsf/globalbackend/models/User.kt
+++ src/main/kotlin/com/stsf/globalbackend/models/User.kt
@@ -5,5 +5,7 @@ package com.stsf.globalbackend.models
 data class User(
     val id: Long,
     val username: String,
-    val password: String
+    val password: String,
+    val firstName: String,
+    val lastName: String
 )
