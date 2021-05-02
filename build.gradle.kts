b782c5f228c84fcb382bcc6c82023665ef174315
Ivan Hrabcak <ivan@hrabcak.eu>
2021-05-02 11:03:14 +0200

    :bug: Adds dialect to configuration files, renames User table

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.google.guava:guava:30.1.1-jre")
+    implementation("org.hibernate:hibernate-core:5.4.31.Final")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
--- src/main/kotlin/com/stsf/globalbackend/models/User.kt
+++ src/main/kotlin/com/stsf/globalbackend/models/User.kt
@@ -1,11 +1,9 @@
 package com.stsf.globalbackend.models
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
+@Table(name="user_table") // the user table name is used by postgres
 data class User(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
