83147e1ad5262b71d238a50e2d92aa4e1dda39c3
Magiccop990 <jtulek@at-rt.com>
2021-05-02 19:10:29 +0200

    :sparkles: Adds services for basic handling of class members

--- src/main/kotlin/com/stsf/globalbackend/repositories/ClassMemberRepository.kt
+++ src/main/kotlin/com/stsf/globalbackend/repositories/ClassMemberRepository.kt
@@ -1,6 +1,11 @@
 package com.stsf.globalbackend.repositories
 
+import com.stsf.globalbackend.models.Class
 import com.stsf.globalbackend.models.ClassMember
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ClassMemberRepository : JpaRepository<ClassMember, Long>
+interface ClassMemberRepository : JpaRepository<ClassMember, Long> {
+	@EntityGraph(value = "User.id")
+	fun findByUserId(id: Long): List<ClassMember>
+}
--- src/main/kotlin/com/stsf/globalbackend/repositories/ClassRepository.kt
+++ src/main/kotlin/com/stsf/globalbackend/repositories/ClassRepository.kt
@@ -7,4 +7,5 @@ interface ClassRepository : JpaRepository<Class, Long> {
 	fun findAllByName(name: String): List<Class>
 	fun findClassById(id: Long): Class
 	fun findAllByNameContains(s: String): List<Class>
+
 }
