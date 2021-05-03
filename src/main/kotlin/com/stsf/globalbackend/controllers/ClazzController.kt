10a33667bd6560bb92975a5ed8a79f26c0f4f0fb
Magiccop990 <jtulek@at-rt.com>
2021-05-03 19:45:49 +0200

    :sparkles: Adds controller for testing

--- src/main/kotlin/com/stsf/globalbackend/controllers/ClazzController.kt
+++ src/main/kotlin/com/stsf/globalbackend/controllers/ClazzController.kt
@@ -2,12 +2,17 @@ package com.stsf.globalbackend.controllers
 
 import com.stsf.globalbackend.services.ClassService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("/api/class")
-class ClazzController (@Autowired classService: ClassService) {
+class ClazzController (@Autowired
+                       private val classService: ClassService) {
 
-	//TODO Finish this crap, i cant do it because IntelliJ's having a stroke and aneurysm rn
+    @GetMapping("/test")
+	fun testClass(@RequestParam className: String) {
+	    classService.createClass(className)
+	}
 
 }
