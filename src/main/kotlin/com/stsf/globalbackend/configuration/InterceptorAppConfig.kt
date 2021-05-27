ccab827147251bf8d57dd0ccef0b4f228a09cf2b
Magiccop990 <jtulek@at-rt.com>
2021-05-27 07:34:29 +0200

    :zap: Adds Logging interceptor to registry

--- src/main/kotlin/com/stsf/globalbackend/configuration/InterceptorAppConfig.kt
+++ src/main/kotlin/com/stsf/globalbackend/configuration/InterceptorAppConfig.kt
@@ -1,6 +1,7 @@
 package com.stsf.globalbackend.configuration
 
 import com.stsf.globalbackend.interceptors.AuthenticationInterceptor
+import com.stsf.globalbackend.logging.LoggingInterceptor
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -8,7 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Component
 class InterceptorAppConfig : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
+        // Authentication Interceptor
         registry.addInterceptor(AuthenticationInterceptor())
             .addPathPatterns("/api/**") // authenticated paths here
+        // Logging Interceptor
+        registry.addInterceptor(LoggingInterceptor())
+            .addPathPatterns("/api/**")
+            // Maybe .excludePatterns("/api/login")?
     }
 }
