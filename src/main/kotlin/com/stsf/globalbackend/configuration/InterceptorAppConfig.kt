17db81a45db6a777417f2e84c5034bfa5feceaa8
Magiccop990 <jtulek@at-rt.com>
2021-06-05 21:22:42 +0200

    :sparkles: Begins work on Request Rate Limiter

--- src/main/kotlin/com/stsf/globalbackend/configuration/InterceptorAppConfig.kt
+++ src/main/kotlin/com/stsf/globalbackend/configuration/InterceptorAppConfig.kt
@@ -1,6 +1,7 @@
 package com.stsf.globalbackend.configuration
 
 import com.stsf.globalbackend.interceptors.AuthenticationInterceptor
+import com.stsf.globalbackend.interceptors.RequestRateLimitInterceptor
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -10,5 +11,7 @@ class InterceptorAppConfig : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(AuthenticationInterceptor())
             .addPathPatterns("/api/**") // authenticated paths here
+        registry.addInterceptor(RequestRateLimitInterceptor())
+            .addPathPatterns("/**")
     }
 }
