a226bde2651f0b15220d45eaaccbdc889a52cf1e
Magiccop990 <jtulek@at-rt.com>
2021-08-07 21:42:39 +0200

    :lock: Fixes security issue with classes Fixes security issue with every teacher being able to modify others' classes

--- src/main/kotlin/com/stsf/globalbackend/controllers/ClazzController.kt
+++ src/main/kotlin/com/stsf/globalbackend/controllers/ClazzController.kt
@@ -22,7 +22,6 @@ class ClazzController (@Autowired
 	fun createClassController(@RequestHeader token: String, @RequestParam className: String): GenericResponse<Class> {
 
 		val authenticatedUser = authenticationService.getUserByToken(token)
-
 		if (!authenticatedUser.isTeacher)
 		{
 			throw InsufficientPermissionsException()
@@ -37,8 +36,13 @@ class ClazzController (@Autowired
 	fun deleteClassController(@RequestHeader token: String, @RequestParam classId: Long): GenericResponse<String> {
 
 		val authenticatedUser = authenticationService.getUserByToken(token)
+		var isUserInClass = classService.getAllUsersInClass(classId).contains(authenticatedUser)
 
-		if (!authenticatedUser.isTeacher) {
+		if (authenticatedUser.isAdmin) {
+			isUserInClass = true
+		}
+
+		if (!authenticatedUser.isTeacher && !isUserInClass) {
 			throw InsufficientPermissionsException()
 		}
 
@@ -61,45 +65,61 @@ class ClazzController (@Autowired
 
 	@PutMapping("/classMember")
 	fun addUserToClassController(@RequestHeader token: String, @RequestBody userAndClassId: UserAndClassId): GenericResponse<ClassMember> {
+		// DONE: Check if teacher is adding students to his class
+		// NOTE: Admins should have the ability to add any user to any class
 
 		val authenticatedUser = authenticationService.getUserByToken(token)
+		var isUserInClass = classService.getAllUsersInClass(userAndClassId.classId).contains(authenticatedUser)
+		val isClassEmpty = classService.getAllUsersInClass(userAndClassId.classId).isEmpty()
 
-		if (!authenticatedUser.isTeacher) {
+		if (authenticatedUser.isAdmin) {
+			isUserInClass = true
+		}
+
+		if ((!authenticatedUser.isTeacher && !isUserInClass) || !isClassEmpty) {
 			throw InsufficientPermissionsException()
 		}
 
-		// TODO: Check if teacher is adding students to his class
-		// NOTE: Admins should have the ability to add any user to any class
 		return GenericResponse(classService.addUserToClass(userAndClassId.userId, userAndClassId.classId))
-
 	}
 
 	@DeleteMapping("/classMember")
 	fun removeUserFromClassController(@RequestHeader token: String, @RequestBody userAndClassId: UserAndClassId): GenericResponse<String> {
+		// DONE: Check if teacher is removing students from his class
+		// NOTE: Admins should have the ability to remove any user from any class
 
 		val authenticatedUser = authenticationService.getUserByToken(token)
+		var isUserInClass = classService.getAllUsersInClass(userAndClassId.classId).contains(authenticatedUser)
 
-		if (!authenticatedUser.isTeacher) {
+		if (authenticatedUser.isAdmin) {
+			isUserInClass = true
+		}
+
+		if (!authenticatedUser.isTeacher && !isUserInClass) {
 			throw InsufficientPermissionsException()
 		}
 
-		// TODO: Check if teacher is removing students from his class
-		// NOTE: Admins should have the ability to remove any user from any class
 		classService.removeUserFromClass(userAndClassId.userId, userAndClassId.classId)
-
 		return GenericResponse("Ok")
 	}
 
 	@GetMapping("/classMember")
 	fun getAllUsersFromClassController(@RequestHeader token: String, @RequestParam classId: Long): GenericResponse<List<User>> {
-		val authenticatedUser = authenticationService.getUserByToken(token)
-		val users = classService.getAllUsersInClass(classId)
+		// DONE: Check if teacher owns this class
+		// NOTE: Admins should have the ability to get users from any class
 
-		if (!authenticatedUser.isTeacher) {
+		val authenticatedUser = authenticationService.getUserByToken(token)
+		var isUserInClass = classService.getAllUsersInClass(classId).contains(authenticatedUser)
+
+		if (authenticatedUser.isAdmin) {
+			isUserInClass = true
+		}
+
+		if (!authenticatedUser.isTeacher && !isUserInClass) {
 			throw InsufficientPermissionsException()
 		}
-		// TODO: Check if teacher owns this class
-		// NOTE: Admins should have the ability to get users from any class
+
+		val users = classService.getAllUsersInClass(classId)
 		return GenericResponse(users)
 	}
 
