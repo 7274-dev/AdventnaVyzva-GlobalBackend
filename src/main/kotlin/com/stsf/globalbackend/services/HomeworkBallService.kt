100b8dfc28bd3b00a086b77a54d22f1df262350c
Ivan Hrabcak <ivan@hrabcak.eu>
2021-11-11 21:27:30 +0100

    :bug: No longer leaks user credentials when creating a new homework ball

--- src/main/kotlin/com/stsf/globalbackend/services/HomeworkBallService.kt
+++ src/main/kotlin/com/stsf/globalbackend/services/HomeworkBallService.kt
@@ -3,10 +3,12 @@ package com.stsf.globalbackend.services
 import com.stsf.globalbackend.exceptions.NoSuchBallException
 import com.stsf.globalbackend.exceptions.NoSuchHomeworkException
 import com.stsf.globalbackend.exceptions.NoSuchUserException
+import com.stsf.globalbackend.models.Homework
 import com.stsf.globalbackend.models.HomeworkBall
 import com.stsf.globalbackend.repositories.HomeworkBallRepository
 import com.stsf.globalbackend.repositories.HomeworkRepository
 import com.stsf.globalbackend.repositories.UserRepository
+import com.stsf.globalbackend.request.GenericResponse
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
@@ -25,13 +27,15 @@ class HomeworkBallService(
         return homeworkBallRepository.getAllByUserId(userId)
     }
 
-    fun addBall(userId: Long, homeworkId: Long): HomeworkBall {
+    fun addBall(userId: Long, homeworkId: Long): GenericResponse<SafeHomeworkBall> {
         val homework = homeworkRepository.findByIdOrNull(homeworkId) ?: throw NoSuchHomeworkException()
         val user = userRepository.findByIdOrNull(userId) ?: throw NoSuchUserException()
-        val ball = HomeworkBall(-1, homework, user)
+        var ball = HomeworkBall(-1, homework, user)
 
-        homeworkBallRepository.save(ball)
-        return ball
+        ball = homeworkBallRepository.save(ball)
+
+        val safeHomeworkBall = SafeHomeworkBall(ball.id, ball.user.id, ball.homework)
+        return GenericResponse(safeHomeworkBall)
     }
 
     fun deleteBallByUserIdAndHomeworkId(userId: Long, homeworkId: Long) {
@@ -47,3 +51,10 @@ class HomeworkBallService(
     }
 
 }
+
+// homework ball without user credentials
+data class SafeHomeworkBall(
+    val id: Long,
+    val userId: Long,
+    val homework: Homework
+)
