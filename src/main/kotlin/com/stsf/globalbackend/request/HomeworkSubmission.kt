5808676c448e2f3dfb1be55d53e1ebce8bb389d5
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2021-11-20 21:14:09 +0100

    :bug: Fixes homework submission

--- src/main/kotlin/com/stsf/globalbackend/request/HomeworkSubmission.kt
+++ src/main/kotlin/com/stsf/globalbackend/request/HomeworkSubmission.kt
@@ -1,8 +1,8 @@
 package com.stsf.globalbackend.request
 
 data class HomeworkSubmission (
-    val id: Long,
-    val user: UserIdAndName,
+    val id: Long?,
+    val user: UserIdAndName?,
     val homeworkId: Long,
     var content: String?,
     val fileIds: List<Long>
